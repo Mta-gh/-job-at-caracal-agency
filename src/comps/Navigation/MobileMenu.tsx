d4e7070d0d204e328ed7a747cbf7bd2c7872cb8f
Mta-gh <mathias.greenhalgh@gmail.com>
2023-01-27 00:15:00 +0100

    menu pop up test

--- src/comps/Navigation/MobileMenu.tsx
+++ src/comps/Navigation/MobileMenu.tsx
@@ -1,16 +1,22 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Component() {
 	const [isOpen, setIsOpen] = useState(false);
 
+	useEffect(() => {
+    setIsOpen(false);
+  }, []);
+
 	return (
 		<>
-			<div className={isOpen ? ' bg-red-100' : ' bg-blue-900 font-jetbrains'}>
+			<div className={isOpen ? ' bg-red-100 is__open' : ' bg-blue-900'}>
 				<button onClick={() => setIsOpen((prev) => !prev)} className="burger__menu w-full h-10 md:hidden" >
 					<span className="burger__line"></span>
 					<span className="burger__line"></span>
 					<span className="burger__line"></span>
 				</button>
+				<div className="test__menu transition-all"></div>
 				{isOpen ? 'Ouvert' : 'Fermé'}
 			</div>
 		</>
