0ebd1fd5e2880277f7b34c82f0625a33d213de43
Mta-gh <mathias.greenhalgh@gmail.com>
2023-01-27 11:52:10 +0100

    menu pop up links

--- src/comps/Navigation/MobileMenu.tsx
+++ src/comps/Navigation/MobileMenu.tsx
@@ -1,23 +1,39 @@
 // import { useState } from 'react';
 import { useState, useEffect } from 'react';
+// import BehermanLogo from '';
+
 
 export default function Component() {
 	const [isOpen, setIsOpen] = useState(false);
 
+	// reset on page reload
 	useEffect(() => {
     setIsOpen(false);
   }, []);
 
 	return (
 		<>
-			<div className={isOpen ? ' bg-red-100 is__open' : ' bg-blue-900'}>
+			<div className={isOpen ? ' is__open' : ' is__closed'}>
 				<button onClick={() => setIsOpen((prev) => !prev)} className="burger__menu w-full h-10 md:hidden" >
 					<span className="burger__line"></span>
 					<span className="burger__line"></span>
 					<span className="burger__line"></span>
 				</button>
-				<div className="test__menu transition-all"></div>
-				{isOpen ? 'Ouvert' : 'Fermé'}
+				<div className="test__menu transition-all flex flex-col items-center justify-center text-5xl">
+				{/* <BehermanLogo /> */}
+
+				<a href="#" className='underlined__links mb-6'>Home</a>
+							<a href="#" className="underlined__links  mb-6">
+								About
+							</a>
+							<a href="#" className="underlined__links  mb-6">
+								History
+							</a>
+							<a href="#" className="underlined__links  mb-6">
+								Philanthropy
+							</a>
+				</div>
+				{/* {isOpen ? 'Ouvert' : 'Fermé'} */}
 			</div>
 		</>
 	);
